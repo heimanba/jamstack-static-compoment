aefeb62f623598345efdd5c745659b0720032296
xsahxl <xsahxl@126.com>
2021-04-09 15:24:32 +0800

    feat: 🎸 oss upload file

--- src/deploy.server.ts
+++ src/deploy.server.ts
@@ -3,17 +3,19 @@ import { spinner } from '@serverless-devs/core';
 import path from 'path';
 import fs from 'fs-extra';
 import { PUT_BUCKET_CORS } from './contants';
+import walkSync from 'walk-sync';
 
 export interface IOssConfig {
-  bucket: string;
-  region: string;
   accessKeyId: string;
   accessKeySecret: string;
+  bucket: string;
+  region: string;
+  staticPath: string;
 }
 
-export default async (ossConfig: IOssConfig, projectName: string) => {
+export default async (ossConfig: IOssConfig) => {
   // 开启OSS上传
-  const { bucket, region, accessKeyId, accessKeySecret } = ossConfig;
+  const { bucket, region, accessKeyId, accessKeySecret, staticPath } = ossConfig;
   // 构造oss客户端
   let ossClient = new OssClient({
     bucket,
@@ -34,21 +36,33 @@ export default async (ossConfig: IOssConfig, projectName: string) => {
   });
 
   // stream方式上传 https://github.com/ali-sdk/ali-oss#putstreamname-stream-options
-  await ossClient.putStream(
-    `source/${projectName}.zip`,
-    fs.createReadStream(path.resolve(process.cwd(), 'build/index.html')),
-  );
+  await put(ossClient, staticPath);
 };
+
+async function put(ossClient: OssClient, staticPath: string) {
+  const paths = walkSync(staticPath);
+  for (const p of paths) {
+    const fillPath = path.resolve(staticPath, p);
+    const stat = fs.statSync(fillPath);
+    if (!stat.isDirectory()) {
+      const spin = spinner(`上传 ${p}`);
+      await ossClient.put(p, fillPath);
+      spin.succeed();
+    }
+  }
+}
 
 async function getOrCreateBucket(ossClient: OssClient, bucket: string) {
   try {
     await ossClient.getBucketInfo(bucket);
     // bucket存在，检查权限，且只能设置为 公共读
     const { acl } = await ossClient.getBucketACL(bucket);
+
     if (acl !== 'public-read') {
       await ossClient.putBucketACL(bucket, 'public-read');
     }
   } catch (error) {
+    // TODO: AccessDenied case
     if (error.code == 'NoSuchBucket') {
       const vm = spinner(`Create ${bucket} bucket`);
       await ossClient.putBucket(bucket);
--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,17 @@ import oss, { IOssConfig } from './deploy.server';
 export default class JamStackComponent {
   @HLogger('JAMSTACK_STATIC') logger: ILogger;
   async deploy(inputs: any) {
-    const { ProjectName, Provider, AccessAlias } = inputs.Project;
+    const { ProjectName, AccessAlias } = inputs.Project;
     this.logger.debug(`[${ProjectName}] inputs params: ${JSON.stringify(inputs, null, 2)}`);
-    const { AccessKeyID, AccessKeySecret } = await getCredential(Provider, AccessAlias);
+    const { AccessKeyID, AccessKeySecret } = await getCredential(AccessAlias);
     const ossConfig: IOssConfig = {
       bucket: get(inputs, 'Properties.bucket'),
       region: get(inputs, 'Properties.region'),
+      staticPath: get(inputs, 'Properties.staticPath', 'build'),
       accessKeyId: get(inputs, 'Credentials.AccessKeyID', AccessKeyID),
       accessKeySecret: get(inputs, 'Credentials.AccessKeySecret', AccessKeySecret),
     };
-
-    await oss(ossConfig, ProjectName);
+    await oss(ossConfig);
   }
 
   async remove(inputs: any) {
